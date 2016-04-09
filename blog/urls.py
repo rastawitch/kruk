1ccde92ae9532103249fb676c0944cae2fd21cf0
Aga <rastawitch18@gmail.com>
2016-04-09 18:46:46 +0200

    last version

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 #zaimportuj lokalną biblioteke views 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
 #r=wyrazenie regularne, ^=ma się zaczynać, $=ma się kończyć, więc ma byc puste, czyli pusta jest część ścieżki po adresie komputera
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 # Create your views here.
@@ -6,7 +6,9 @@ def post_list(request):
 	posts=Post.objects.all() #pobieranie danych z bazy danych
 	return render(request, 'blog/post_list.html', {'posts':posts}) #przekazywanie posts do templatu,ale on jeszcze z tego nie korzysta, wrzycam ją w post list w 
     
-
+def post_detail(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post': post})
 
 #return zwraca to, co jest wywołaniem kolejnej funkcji (render)
 #patrz urls.py blog
